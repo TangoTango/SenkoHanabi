a10e0564a03a3b8310ab06b61b858844db959371
TangoTango <tkytango@gmail.com>
2013-09-05 21:51:20 +0900

    Initial Commit

---
+++ SenkoHanabiARC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SenkoHanabiARC
+//
+//  Created by 丹後 偉也 on 2013/09/05.
+//  Copyright (c) 2013年 PTA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SenkoHanabiARC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SenkoHanabiARC
+//
+//  Created by 丹後 偉也 on 2013/09/05.
+//  Copyright (c) 2013年 PTA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
